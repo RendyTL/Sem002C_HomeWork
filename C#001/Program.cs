54c5d3ddf8e8bf659c6ff0179edca3519eeb73b2
Sofia <lucifersatandf@gmail.com>
2023-06-18 01:28:07 +0300

    1

---
+++ C#001/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+int RandomNumber = new Random().Next(0, 1000);
+Console.WriteLine(RandomNumber);
+
+int rightNumber = RandomNumber / 10;
+int rightNumber2 = rightNumber % 10;
+
+Console.WriteLine(rightNumber2);
